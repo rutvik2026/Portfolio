90b144bc4ee795e033a9106f506fdb94ec3d2577
Aviraj <shete.adarsh@gmail.com>
2025-03-31 09:35:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+let sections=document.querySelectorAll('section')
+let navLinks=document.querySelectorAll('header nav a')
+window.onscroll=()=>{
+    sections.forEach(sec =>{
+    let top=window.scrollY;
+    let offset=sec.offsetTop-100;
+    let height=sec.offsetHeight;
+    let id=sec.getAttribute('id');
+    if(top>=offset && top<offset+height){
+        navLinks.forEach(links=>{
+            links.classList.remove('active');
+            document.querySelector('header nav a[href*='+id+']').classList.add('active');
+        });
+    }
+});
+    let header=document.querySelector('header');
+header.classList.toggle('sticky',window.scrollY > 100);
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var options = {
+        strings: ["Java Devloper", "Web Devloper","Android Devloper","python basics","App Devloper"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        loop:true
+    };
+
+    // Ensure you use `Typed` (with capital 'T') and not `typed`
+    var typed = new Typed(".text", options);
+});
+   
